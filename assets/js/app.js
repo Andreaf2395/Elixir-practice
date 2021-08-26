38952a51e71b1bcac5c6ae572926c95e7cee1e6a
Andrea Furtado <andreaf2395@gmail.com>
2021-08-26 14:00:10 +0530

    pheonix basic

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+// We need to import the CSS so that webpack will load it.
+// The MiniCssExtractPlugin is used to separate it out into
+// its own CSS file.
+import "../css/app.css"
+
+// webpack automatically bundles all modules in your
+// entry points. Those entry points can be configured
+// in "webpack.config.js".
+//
+// Import deps with the dep name or local files with a relative path, for example:
+//
+    import {Socket, Presence} from "phoenix"
+    // import socket from "./socket"
+//
+import "phoenix_html"
+
+let socket = new Socket("/socket", {
+    params: {user_id: window.location.search.split("=")[1]}
+  })
+  
+  let channel = socket.channel("room:lobby", {})
+  let presence = new Presence(channel)
+  
+  function renderOnlineUsers(presence) {
+    let response = ""
+  
+    presence.list((id, {metas: [first, ...rest]}) => {
+      let count = rest.length + 1
+      response += `<br>${id} (count: ${count})</br>`
+    })
+  
+    document.querySelector("main[role=main]").innerHTML = response
+  }
+  
+  socket.connect()
+  
+  presence.onSync(() => renderOnlineUsers(presence))
+  
+  channel.join()
---
+++ lib/hello_web/channels/room_channel.ex
@@ -0,0 +1,31 @@
+defmodule HelloWeb.RoomChannel do
+  use Phoenix.Channel
+  alias HelloWeb.Presence
+
+
+  def join("room:lobby", _params, socket) do
+    send(self(), :after_join)
+    {:ok, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    {:ok, _} = Presence.track(socket, socket.assigns.user_id, %{
+      online_at: inspect(System.system_time(:second))
+    })
+
+    push(socket, "presence_state", Presence.list(socket))
+    {:noreply, socket}
+  end
+
+  # def join("room:lobby", _message, socket) do
+  #   {:ok, socket}
+  # end
+  # def join("room:" <> _private_room_id, _params, _socket) do
+  #   {:error, %{reason: "unauthorized"}}
+  # end
+
+  # def handle_in("new_msg", %{"body" => body}, socket) do
+  #   broadcast!(socket, "new_msg", %{body: body})
+  #   {:noreply, socket}
+  # end
+end
